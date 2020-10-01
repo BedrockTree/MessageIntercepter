42142ff7db12688adbd9295c8484944112c9acf1
Frank <frank2007shandong@outlook.com>
2020-10-01 20:04:34 +0800

    A bukkit plugin

--- src/com/frank/main/command/AddIntercept.java
+++ src/com/frank/main/command/AddIntercept.java
@@ -18,22 +18,27 @@ public class AddIntercept implements CommandExecutor {
         @Override
         public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
                 if (strings.length == 1) {
-                        FileConfiguration config = plugin.getConfig();
-                        List<String> temp = config.getStringList("InterceptList");
-                        temp.add(strings[0]);
-                        config.set("InterceptList",temp);
-                        plugin.saveConfig();
-                        if (commandSender instanceof Player) {
-                                commandSender.sendMessage(ChatColor.RED + "成功将" + strings[0] + "加入到屏蔽词列表");
-                        } else {
-                                plugin.getLogger().info("成功将" + strings[0] + "加入到屏蔽词列表");
+                        if (commandSender.hasPermission("op")){
+                                FileConfiguration config = plugin.getConfig();
+                                List<String> temp = config.getStringList("InterceptList");
+                                temp.add(strings[0]);
+                                config.set("InterceptList",temp);
+                                plugin.saveConfig();
+                                if (commandSender instanceof Player) {
+                                        commandSender.sendMessage(ChatColor.RED + "成功将" + strings[0] + "加入到屏蔽词列表");
+                                } else {
+                                        plugin.getLogger().info("成功将" + strings[0] + "加入到屏蔽词列表");
+                                }
+                                Bukkit.broadcastMessage(ChatColor.RED +commandSender.getName() + "将" + strings[0] + "加入了屏蔽词列表，请注意\n");
+                                plugin.getLogger().warning(commandSender.getName() + "将" + strings[0] + "加入了屏蔽词列表");
+                                plugin.saveConfig();
+                                plugin.saveDefaultConfig();
+                                plugin.reloadConfig();
+                                return true;
+                        }else{
+                                commandSender.sendMessage(ChatColor.RED + "你没有执行该指令的权限（MessageIntercepter.commands.AddIntercept）");
+                                return false;
                         }
-                        Bukkit.broadcastMessage(ChatColor.RED +commandSender.getName() + "将" + strings[0] + "加入了屏蔽词列表，请注意\n");
-                        plugin.getLogger().warning(commandSender.getName() + "将" + strings[0] + "加入了屏蔽词列表");
-                        plugin.saveConfig();
-                        plugin.saveDefaultConfig();
-                        plugin.reloadConfig();
-                        return true;
                 } else {
                         return false;
                 }
